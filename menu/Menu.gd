281101b4192030370ad6fc09a226f7d33b314f4d
Grigi <nagrigoriadis@gmail.com>
2021-06-23 14:52:02 +0200

    Menu enemy animation

--- menu/Menu.gd
+++ menu/Menu.gd
@@ -1,26 +1,22 @@
 extends Node2D
 
-
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var time = 0.0
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+#func _ready():
+#	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+func _process(delta):
+	time += delta
+	$enemy.position.y = sin(time*1.1)*100 + 290
+	$enemy.position.x = cos(time*1.6)*20 + 888
 
 
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://game/game.tscn")
-	pass # Replace with function body.
 
 
 func _on_QuitButton_pressed():
