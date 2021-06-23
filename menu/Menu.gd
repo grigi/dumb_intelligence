752497ac53aecc1427f788f12277cb2b13346932
Grigi <nagrigoriadis@gmail.com>
2021-06-23 21:25:36 +0200

    Added Menu sounds

--- menu/Menu.gd
+++ menu/Menu.gd
@@ -11,13 +11,17 @@ var time = 0.0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	time += delta
-	$enemy.position.y = sin(time*1.1)*100 + 290
-	$enemy.position.x = cos(time*1.6)*20 + 888
+	$Enemy.position.y = sin(time*1.1)*100 + 290
+	$Enemy.position.x = cos(time*1.6)*20 + 888
 
 
 func _on_PlayButton_pressed():
-	get_tree().change_scene("res://game/game.tscn")
+	$Click.play()
+	yield(get_tree().create_timer(0.25), "timeout")
+	var _res = get_tree().change_scene("res://game/game.tscn")
 
 
 func _on_QuitButton_pressed():
+	$Click.play()
+	yield(get_tree().create_timer(0.25), "timeout")
 	get_tree().quit()
